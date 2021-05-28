e5414ddfc1a417ad1c9a35bf67fbcd6cf9df435f
JR-Morgan <45512892+JR-Morgan@users.noreply.github.com>
2021-05-28 03:15:07 +0100

    Removed DOTS packages

--- Assets/Scripts/DOTS AI/Extensions/Math Extensions.cs
+++
@@ -1,9 +0,0 @@
-using Unity.Mathematics;
-
-namespace DOTS_AI.Extensions
-{
-    public static class MathE
-    {
-        public static float Angle(in float3 a, in float3 b) => math.acos(math.dot(a, b) / math.dot(math.lengthsq(a), math.lengthsq(b)));
-    }
-}
